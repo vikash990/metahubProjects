99b715ac6f27c2f37a4eed6cd54f517c9712f658
unknown <vikash990>
2023-06-13 19:30:39 +0530

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+var x=20;  // it is a container to hold a value
+// ways to show the output 
+
+console.log(x);  // debugging 
+alert(x);   // warning
+document.write(x); // 
+
+document.getElementById("one").innerHTML=x;  // use to manipulate data
+
+// data type 
+
+var name="vikash dubey"   // anything inside a double quotes is called a string
+var num=90       // number
+var condition=false  // boolean  true or false
+var t;  // declaration 
+console.log(t)
+
+
+console.log(name,typeof(name))
+console.log(num,typeof(num))
+console.log(condition,typeof(condition))
+console.log(t,typeof(t))
+
+console.log(name,num,condition)
